968d96e2a7cf870061779d28f0ba0dd86401e1a8
khalil <khalil@khalil-PC>
2016-10-11 12:28:47 +0100

    First Commit

---
+++ src/beans/Personne.java
@@ -0,0 +1,36 @@
+package beans;
+
+public class Personne {
+
+	int num;
+	String nom;
+	String prenom;
+	int age;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
