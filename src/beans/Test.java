d8d594b7612877d8981cd95c3cc4de2bffac4aff
khalil0M <khalil.kouiss@gmail.com>
2016-10-12 14:09:26 +0100

    Create Test.java

---
+++ src/beans/Test.java
@@ -0,0 +1,48 @@
+package beans;
+
+public class Test {
+
+	int id ;
+	String nom;
+	String prenon;
+	String adress;
+	String societe;
+	
+	//class Client  
+	//class Client
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenon() {
+		return prenon;
+	}
+	public void setPrenon(String prenon) {
+		this.prenon = prenon;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getSociete() {
+		return societe;
+	}
+	public void setSociete(String societe) {
+		this.societe = societe;
+	}
+	
+	
+	
+	
+}
